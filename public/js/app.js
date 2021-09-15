869a656d08674e8f9e6546d8b43783e1c034b81c
Dave Stanley <dave@pop-os.localdomain>
2021-09-15 19:10:59 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,14 +13,12 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        data.error
-          ? (messageOne.textContent = data.error)
-          : (messageOne.textContent = data.location),
-          (messageTwo.textContent = data.forecast);
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      data.error
+        ? (messageOne.textContent = data.error)
+        : (messageOne.textContent = data.location),
+        (messageTwo.textContent = data.forecast);
+    });
+  });
 });
