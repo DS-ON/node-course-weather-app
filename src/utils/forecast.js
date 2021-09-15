7c16fc162c00965f6e0e21d7d4b5fe34e9431c90
Dave Stanley <dave@pop-os.localdomain>
2021-09-15 19:38:09 -0400

    Added new info to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("postman-request");
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=9609760d16be21a9acc182288db4421f&query=${latitude},${longitude}&units=f`;
+  const url = `http://api.weatherstack.com/current?access_key=9609760d16be21a9acc182288db4421f&query=${latitude},${longitude}&units=m`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -12,7 +12,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
       );
     }
   });
